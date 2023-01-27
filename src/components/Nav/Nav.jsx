abcde84efc90e84341049d442e8798f9a0ad82c6
Gianella Leon <giigl@hotmail.com>
2023-01-26 22:47:17 -0500

    center nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-// CUIDADOOOO. SI O SI CLASS COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
+import './Nav.css' 
 class Nav extends Component {
 
     render() {
         return (
-            <div>
-                <Link to='/'> 
-                    <h4>Home</h4>
-                </Link>
-                <Link to='/house/create'>
-                    <h4>Create House</h4>
-                </Link>
+            <div className='navCont'>
+                <div className='navTab'>
+                    <Link to='/'> 
+                        <h4 >Home</h4>
+                    </Link>
+                </div>
+                <div  className='navTab'>
+                    <Link to='/house/create'>
+                        <h4>Create House</h4>
+                    </Link>
+                </div>
             </div>
         );
     };
